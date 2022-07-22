07e179dd4e190b507d2d0947c1d187350c29ab80
dcode-denpa <denisjuliandra01@gmail.com>
2022-07-22 19:14:23 +0700

    stumble-hack

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const fetch = require('node-fetch');
 const moment = require('moment');
 const chalk = require('chalk');
 const fs = require('fs');
-function turu(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
 const StumbleHack = (auth) => new Promise((resolve, reject) => {fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {method: 'GET',headers: {'authorization': auth}}).then(res => res.text()).then(data => {resolve(data);}).catch(err => {reject(err);});});
 (async() => {
+console.clear();
 console.log(chalk.black(`${chalk.cyan(`░██████╗████████╗██╗░░░██╗███╗░░░███╗██████╗░██╗░░░░░███████╗  ██╗░░██╗░█████╗░░█████╗░██╗░░██╗
 ██╔════╝╚══██╔══╝██║░░░██║████╗░████║██╔══██╗██║░░░░░██╔════╝  ██║░░██║██╔══██╗██╔══██╗██║░██╔╝
 ╚█████╗░░░░██║░░░██║░░░██║██╔████╔██║██████╦╝██║░░░░░█████╗░░  ███████║███████║██║░░╚═╝█████═╝░
@@ -15,12 +15,12 @@ while (true) {
 const result = await StumbleHack(fs.readFileSync('./auth.json'));
 if (!result) { console.log(chalk.red('Authentication Code Not Valid!'));
 break;
+} else if (result.includes('User')) {
 const data = JSON.parse(result);
-console.log(chalk.bgBlack(`${chalk.yellow(`Id : ${data.User.Id} - Version : ${data.User.Version} - Username : ${data.User.Username} - Country : ${data.User.Country}`)}\n${chalk.magenta(`Time : ${moment().format('HH:mm:ss')}`)} | ${chalk.blue(`Success Added Trophy : ${data.User.SkillRating}`)} | ${chalk.green(`Success Added Crown : ${data.User.Crowns}`)}`));
-await turu(3000);
+const trophy = data.User.SkillRating;
+const crown = data.User.Crowns;
+console.log(chalk.bgBlack(`${chalk.yellow(`Id : ${data.User.Id} - Version : ${data.User.Version} - Username : ${data.User.Username} - Country : ${data.User.Country}`)}\n${chalk.magenta(`Time : ${moment().format('HH:mm:ss')}`)} | ${chalk.blue(`Success Added Trophy : ${trophy}`)} | ${chalk.green(`Success Added Crown : ${crown}`)}`));
 } else if (result == 'BANNED') { console.log(chalk.bgRed('BANNED!!!'));
 break;
-} else if (result == 'SERVER_ERROR' || result.includes('User path limit exceeded')) {
-continue;
 } else { console.log(chalk.bgRed('ERROR!!!'));
 break;}}})();
